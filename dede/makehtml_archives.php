087988cbee466af4855917301616b2b32977285e
DedeCMS Team <mycalf@me.com>
2020-11-10 16:12:45 +0800

    init dedecms 5.x.

---
+++ dede/makehtml_archives.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * 生成文档
+ *
+ * @version        $Id: makehtml_archives.php 1 9:10 2010年7月19日 $
+ * @package        DedeCMS.Administrator
+ * @copyright      Copyright (c) 2007 - 2020, DesDev, Inc.
+ * @license        http://help.dedecms.com/usersguide/license.html
+ * @link           http://www.dedecms.com
+ */
+require_once(dirname(__FILE__)."/config.php");
+require_once(DEDEINC."/typelink.class.php");
+include DedeInclude('templets/makehtml_archives.htm');
---
+++ dede/makehtml_list.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * 生成列表栏目
+ *
+ * @version        $Id: makehtml_list.php 1 11:09 2010年7月19日 $
+ * @package        DedeCMS.Administrator
+ * @copyright      Copyright (c) 2007 - 2020, DesDev, Inc.
+ * @license        http://help.dedecms.com/usersguide/license.html
+ * @link           http://www.dedecms.com
+ */
+require_once(dirname(__FILE__)."/config.php");
+require_once(DEDEINC."/typelink.class.php");
+include DedeInclude('templets/makehtml_list.htm');
